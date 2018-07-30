443144e433ec032cfb21da56f368286e683359a8
benjamin boumendil <benjamin.boumendil@schibsted.com>
2018-07-30 15:55:19 +0200

    update database used by tests

--- dataf/tests/__init__.py
+++ dataf/tests/__init__.py
@@ -1,6 +1,6 @@
 class settings:
     DATABASE = {
         'test': {
-            'url': 'postgresql://admin:password@localhost:5432/hackathon'
+            'url': 'postgresql://admin:password@localhost:5432/test'
         }
     }
